7a07b62c563aedd149b5ec4200a3d8f56319faca
Marium Umair <160335280+mariumumair7@users.noreply.github.com>
2024-12-23 00:17:31 +0500

    my files

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -13,7 +13,13 @@ const Header = () => {
           {/* Search Button */}
           <li className="flex items-center">
             <Link href="/search">
-              <Image src="/1.png" alt="Search" width={20} height={20} className="hover:bg-gray-200 rounded-full" />
+              <Image
+                src="/1.png"
+                alt="Search"
+                width={20}
+                height={20}
+                className="hover:bg-gray-200 rounded-full"
+              />
             </Link>
           </li>
 
@@ -27,17 +33,29 @@ const Header = () => {
           {/* Cart and User Buttons */}
           <li className="flex items-center space-x-4">
             <Link href="/cart">
-              <Image src="/cart.jpeg" alt="Cart" width={20} height={20} className="hover:bg-gray-200 rounded-full" />
+              <Image
+                src="/cart.jpeg"
+                alt="Cart"
+                width={20}
+                height={20}
+                className="hover:bg-gray-200 rounded-full"
+              />
             </Link>
             <Link href="/user-profile">
-              <Image src="/user.jpeg" alt="User Profile" width={20} height={20} className="hover:bg-gray-200 rounded-full" />
+              <Image
+                src="/user.jpeg"
+                alt="User Profile"
+                width={20}
+                height={20}
+                className="hover:bg-gray-200 rounded-full"
+              />
             </Link>
           </li>
         </ul>
       </div>
 
       {/* Navigation Links */}
-      <div className="absolute top-[90px] left-[382px] w-[675px] h-[22px] flex gap-[44px] text- medium text-DarkPrimary">
+      <div className="absolute top-[90px] left-[382px] w-[675px] h-[22px] flex gap-[44px] text-lg text-DarkPrimary">
         <Link href="/plant-pots">Plant Pots</Link>
         <Link href="/ceramics">Ceramics</Link>
         <Link href="/tables">Tables</Link>
